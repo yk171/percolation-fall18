a735a5a7608383cfca4a48fc028c55957645a1d5
Eugene Kim <yk171@duke.edu>
2018-12-05 10:24:34 -0500

    class

---
+++ src/PercolationDFSFast.java
@@ -0,0 +1,20 @@
+
+public class PercolationDFSFast extends PercolationDFS {
+		
+		public PercolationDFSFast(int size) {
+			super(size);
+		}
+		@Override
+		protected void updateOnOpen(int row, int col) {
+			
+			for (int k = 0; k < myGrid[0].length; k++) {
+				if(isFull(0,k))dfs(0, k);
+				if(isFull(row+1, k-1)) dfs(row,col);
+				if(isFull(row-1, k-1)) dfs(row,col);
+				if(isFull(row-1, k+1)) dfs(row,col);
+				if(isFull(row+1, k+1)) dfs(row,col);
+				
+				
+			}
+		}
+	}
