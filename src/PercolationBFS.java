c40d688c43fe61f181b49af42c061699b4525d45
Eugene Kim <yk171@duke.edu>
2018-12-05 19:59:25 -0500

    hi

--- src/PercolationBFS.java
+++ src/PercolationBFS.java
@@ -6,7 +6,7 @@ public class PercolationBFS extends PercolationDFSFast{
 	}
 	@Override
 	protected void dfs(int row, int col) {
-		int size = 0;
+		int size = myGrid.length;
 		int[] rowDelta = {-1,1,0,0};
 	    int[] colDelta = {0,0,-1,1};
 	    
@@ -18,14 +18,16 @@ public class PercolationBFS extends PercolationDFSFast{
 		Queue<Integer> qp = new LinkedList<>();
 		myGrid[row][col] = FULL;
 		size++;
-		qp.add(myGrid[row][col]);
+		
+		qp.add(a);
+		
 		while(qp.size() != 0) {
 			Integer p = qp.remove();
 			for(int i = 0; i < rowDelta.length; i++) {
 				row = p/size + rowDelta[i];
 				col = p%size + colDelta[i];
 				if(inBounds(row, col) && myGrid[row][col] == FULL && myGrid[row][col] == OPEN) {
-						qp.add(myGrid[row][col]);
+						qp.add(a);
 						myGrid[row][col] = FULL;
 						size++;
 				}
