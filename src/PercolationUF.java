e94dce149e0c4e1a6cf4d80b5fdf08faef389f46
Eugene Kim <yk171@duke.edu>
2018-12-06 23:18:42 -0500

    woah

--- src/PercolationUF.java
+++ src/PercolationUF.java
@@ -6,7 +6,7 @@ public class PercolationUF implements IPercolate{
 	private final int VTOP;
 	private final int VBOTTOM;
 	
-	public PercolationUF(IUnionFind finder, int size) {
+	public PercolationUF(int size, IUnionFind finder) {
 		myGrid = new boolean[size][size];
 		VTOP = size*size;
 		VBOTTOM = size*size + 1;
